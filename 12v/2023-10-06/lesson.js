5d43ffffd99165e06ef5caedb5d01b2e71f2b69b
mdatsev <martin.datsev.p@gmail.com>
2023-10-05 20:59:00 +0300

    add lesson

---
+++ 12v/2023-10-06/lesson.js
@@ -0,0 +1,73 @@
+// Code structure
+
+// Statements
+
+// Semicolons
+
+// Comments
+
+// Strict mode - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+
+// Variables
+
+// Data types
+  // Primitive - unefined, number, bigint, boolean, string, symbol
+  // Non-primitive - object
+
+// Type conversions
+  // String Conversion
+  // Numeric Conversion
+  // Boolean Conversion
+
+// Operators
+  // Numeric + - * / % ** +
+  // String +
+  // ++  --
+  // Bitwise & | ^ ~ << >> >>>
+  // Comparisons < > <= >= == != === !==
+  // Be careful with automatic type conversions!
+  // Logical || && ! ??
+
+// Conditionals - if, ?:, switch
+
+// Loops - while, do while, for
+
+// Functions
+  // Declaration
+  // Parameters
+  // Default parameters
+  // Function expressions
+  // IIFEs
+  // Callbacks
+  // Arrow functions
+
+// Objects
+  // Object syntax and property access
+  // in operator
+  // for..in
+  // Object references and copying
+  // Object == and ===
+  // const objects
+  // structuredClone
+  // Optional chaining '?.'
+
+// Arrays
+  // Array syntax
+  // Array methods - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+  // Arrays are objects
+  // for..of
+  // Object.keys(), Object.values(), Object.entries()
+
+// Destructuring
+  // assignment
+  // function parameter and default
+  // rest and spread
+
+// JSON, JSON methods
+
+// Error handling
+  // try catch finally
+  // global catch - Environment-specific
+
+
+// Callbacks and promises - next time
