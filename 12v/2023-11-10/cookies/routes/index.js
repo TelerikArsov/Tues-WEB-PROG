cf6f327b5bc7e4a7fe5d00ddb6715c214d808c37
mdatsev <martin.datsev.p@gmail.com>
2023-11-10 15:08:31 +0200

    cookies

---
+++ 12v/2023-11-10/cookies/routes/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: req.cookies.preference });
+});
+
+/* GET users listing. */
+router.post("/", function (req, res, next) {
+  res.cookie("preference", req.body.preference);
+  res.redirect("/");
+});
+module.exports = router;
