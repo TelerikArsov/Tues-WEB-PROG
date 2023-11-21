476be8e00e1789cfbfdf41caec2099d96c3fd997
mdatsev <martin.datsev.p@gmail.com>
2023-11-21 12:55:23 +0200

    ssr and csr example

--- 12v/2023-11-17/example/routes/index.js
+++ 12v/2023-11-17/example/routes/index.js
@@ -4,7 +4,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  res.render("index", { title: "Posts" });
+  const posts = await getPosts();
+  res.render("index", { title: "Posts", posts });
+});
+
+router.post("/", async function (req, res, next) {
+  const { title, content } = req.body;
+  await createPost(content, title);
+  res.redirect("/");
+});
+
+router.get("/post/:id", async function (req, res, next) {
+  const post = await getPostById(req.params.id);
+  res.render("post", post);
+});
+
+router.get("/something2", async function (req, res, next) {
+  res.send("Something in index router");
 });
 
 module.exports = router;
